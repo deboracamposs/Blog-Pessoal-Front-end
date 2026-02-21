1cf66bf930524925cf92111ee119dae210ae6cc6
unknown <deboraccamposs@gmail.com>
2026-02-21 18:34:44 -0300

    Form Tema

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -23,7 +23,7 @@ import { useContext } from "react";
                     <div className = "flex gap-4">
                         Postagens 
                         <Link to='/temas' className='hover:underline'>Temas</Link> 
-                        Cadastrar tema 
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar Tema</Link>  
                         Perfil 
                         <Link to='' onClick={logout} className="hover:underline">Sair</Link>
                     </div>
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,12 +4,17 @@ const api = axios.create({
     baseURL: 'https://blog-pessoal-4085.onrender.com'
 });
 
-export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
+export const cadastrarUsuario = async(url: string, dados: object, setDados: Function) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data);
 }
 
-export const login = async(url: string, dados: Object, setDados: Function) => {
+export const login = async(url: string, dados: object, setDados: Function) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data);
+}
+
+export const buscar = async (url: string, setDados: Function, header: object) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
 }
